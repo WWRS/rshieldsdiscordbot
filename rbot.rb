b3d254ffff6d3c852d2c92809253ea2135077537
RShields <playnow254@gmail.com>
2017-07-12 05:13:22 +0900

    Do the thing

--- rbot.rb
+++ rbot.rb
@@ -1,4 +1,30 @@
+
+def linux_variant
+  r = { :distro => nil, :family => nil }
+
+  if File.exists?('/etc/lsb-release')
+    File.open('/etc/lsb-release', 'r').read.each_line do |line|
+      r = { :distro => $1 } if line =~ /^DISTRIB_ID=(.*)/
+    end
+  end
+
+  if File.exists?('/etc/debian_version')
+    r[:distro] = 'Debian' if r[:distro].nil?
+    r[:family] = 'Debian' if r[:variant].nil?
+  elsif File.exists?('/etc/redhat-release') or File.exists?('/etc/centos-release')
+    r[:family] = 'RedHat' if r[:family].nil?
+    r[:distro] = 'CentOS' if File.exists?('/etc/centos-release')
+  elsif File.exists?('/etc/SuSE-release')
+    r[:distro] = 'SLES' if r[:distro].nil?
+  end
+
+  return r
+end
+p linux_variant
+
+
 require'rbnacl/libsodium'
+require'opus-ruby'
 require'discordrb'
 
 tokentxt='Mjg1NjQ5NTg3Njg2MDgwNTIz.C5VPbw.eBMnABqxYGSwbTS8VxKXqF7WukE'
