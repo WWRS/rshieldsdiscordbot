7515364929defef00bb0fa5510e1a14b2e1bb473
RShields <playnow254@gmail.com>
2017-07-13 00:49:16 +0900

    Do it

--- rbot.rb
+++ rbot.rb
@@ -2,65 +2,28 @@ require'bundler/setup'
 require'rbnacl/libsodium'
 
 require'ffi'
-require'opus-ruby'
-require'opus-ruby/version'
-require'opus-ruby/encoder'
-require'opus-ruby/decoder'
 module Rtricksopus
-	extend FFI::Library
-	
-	opus_dir = File.expand_path("..", __FILE__)
-	p opus_dir
-	opus_glob = case RUBY_DESCRIPTION
-		when /darwin/ then "opus*.dylib"
-		when /Windows|(win|mingw)32/ then "opus*.dll"
-		else "opus*.so"
+	def ffi_lib(*names)
+		if names == ['opus']
+			opus_dir = File.expand_path("..", __FILE__)
+			p opus_dir
+			opus_glob = case RUBY_DESCRIPTION
+				when /darwin/ then "opus*.dylib"
+				when /Windows|(win|mingw)32/ then "opus*.dll"
+				else "opus*.so"
+			end
+			loc = Dir.glob(File.join(opus_dir, opus_glob)).first
+			p loc
+			names = [loc]
+		end
+		super(names)
 	end
-	loc = Dir.glob(File.join(opus_dir, opus_glob)).first
-	p loc
-	ffi_lib loc
-	
-	module Constants
-		OPUS_OK                               = 0
-		OPUS_BAD_ARG                          = -1
-		OPUS_BUFFER_TOO_SMALL                 = -2
-		OPUS_INTERNAL_ERROR                   = -3
-		OPUS_INVALID_PACKET                   = -4
-		OPUS_UNIMPLEMENTED                    = -5
-		OPUS_INVALID_STATE                    = -6
-		OPUS_ALLOC_FAIL                       = -7
-		OPUS_APPLICATION_VOIP                 = 2048
-		OPUS_APPLICATION_AUDIO                = 2049
-		OPUS_APPLICATION_RESTRICTED_LOWDELAY  = 2051
-		OPUS_SIGNAL_VOICE                     = 3001
-		OPUS_SIGNAL_MUSIC                     = 3002
-		OPUS_SET_BITRATE_REQUEST              = 4002
-		OPUS_SET_VBR_REQUEST                  = 4006
-		OPUS_RESET_STATE                      = 4028
-	end
-	
-	attach_function :opus_encoder_get_size, [:int], :int
-	attach_function :opus_encoder_create, [:int32, :int, :int, :pointer], :pointer
-	attach_function :opus_encoder_init, [:pointer, :int32, :int, :int], :int
-	attach_function :opus_encode, [:pointer, :pointer, :int, :pointer, :int32], :int32
-	attach_function :opus_encode_float, [:pointer, :pointer, :int, :pointer, :int32], :int32
-	attach_function :opus_encoder_destroy, [:pointer], :void
-	attach_function :opus_encoder_ctl, [:pointer, :int, :varargs], :int
-	
-	attach_function :opus_decoder_get_size, [:int], :int
-	attach_function :opus_decoder_create, [:int32, :int, :pointer], :pointer
-	attach_function :opus_decoder_init, [:pointer, :int32, :int], :int
-	attach_function :opus_decode, [:pointer, :pointer, :int32, :pointer, :int, :int], :int
-	attach_function :opus_decode_float, [:pointer, :pointer, :int32, :pointer, :int, :int], :int
-	attach_function :opus_decoder_ctl, [:pointer, :int, :varargs], :int
-	attach_function :opus_decoder_destroy, [:pointer], :void
 end
-module Opus
+module FFI::Library
 	prepend Rtricksopus
 end
 
 require'discordrb'
-
 module Rtrickscommandbot
 	def execute_command(name, event, arguments, chained = false, check_permissions = true)
 		super(name.downcase.to_sym, event, arguments, chained, check_permissions)
